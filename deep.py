fe2b86c14d7c28573c9dadf85cb7d92ef8b7a9e6
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-06 02:02:35 +0000

    automated commit by check50 [check50=True]

--- deep.py
+++ deep.py
@@ -2,11 +2,11 @@
 response = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
 # If the response is 42, Forty Two, or Forty-Two then print yes
-if response == "42":
+if response.strip() == "42":
     print("Yes")
-elif response == "forty two":
+elif response.islower().strip() == "forty two":
     print("Yes")
-elif response == "forty-two":
+elif response.islower().strip() == "forty-two":
     print("Yes")
 # Otherwise the default case will print no
 else:
