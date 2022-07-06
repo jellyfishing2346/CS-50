753f0109fb57f029fe76ae08ea3417cdb673d979
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-06 15:27:30 +0000

    automated commit by check50 [check50=True]

--- bank.py
+++ bank.py
@@ -1,23 +1,20 @@
 # Prompt the user to enter a greeting
 response = input("Greeting: " )
 
-# New response to encounter user modifications
-new_response = response.strip().lower()
-
 # If the user enters Hello, print $0
-if new_response == "Hello":
+if response == "Hello":
     print("$0")
 
 # If the user enters Hello, Newman prints $0
-elif new_response == "Hello, Newman":
+elif response == "Hello, Newman":
     print("$0")
 
 # If the user enter How you doing?, prints $20
-elif new_response == "How you doing?":
+elif response == "How you doing?":
     print("$20")
 
 # If the user enter What's happening?, print $100
-elif new_response == "What's happening?":
+elif response == "What's happening?":
     print("$100")
 
 # If the user enters other case by default print $100
