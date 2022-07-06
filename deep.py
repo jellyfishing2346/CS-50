422900fb2da444d2032fcc74785667589ec4b7a2
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-06 02:00:17 +0000

    automated commit by check50 [check50=True]

--- deep.py
+++ deep.py
@@ -4,7 +4,7 @@ response = input("What is the Answer to the Great Question of Life, the Universe
 # If the response is 42, Forty Two, or Forty-Two then print yes
 if response == "42":
     print("Yes")
-elif response == "Forty Two":
+elif response == "forty two":
     print("Yes")
 elif response == "forty-two":
     print("Yes")
