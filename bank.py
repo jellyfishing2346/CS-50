5c0c1336a697d1da987d416a6814c0baffe0f852
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-06 15:25:46 +0000

    automated commit by check50 [check50=True]

--- bank.py
+++ bank.py
@@ -2,22 +2,22 @@
 response = input("Greeting: " )
 
 # New response to encounter user modifications
-
+new_response = response.strip().lower()
 
 # If the user enters Hello, print $0
-if response == "Hello":
+if new_response == "Hello":
     print("$0")
 
 # If the user enters Hello, Newman prints $0
-elif response == "Hello, Newman":
+elif new_response == "Hello, Newman":
     print("$0")
 
 # If the user enter How you doing?, prints $20
-elif response == "How you doing?":
+elif new_response == "How you doing?":
     print("$20")
 
 # If the user enter What's happening?, print $100
-elif response == "What's happening?":
+elif new_response == "What's happening?":
     print("$100")
 
 # If the user enters other case by default print $100
