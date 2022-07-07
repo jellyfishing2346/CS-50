cd763b615298867aeb1edc28b245c79db59ea227
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-07 15:02:48 +0000

    automated commit by check50 [check50=True]

--- meal.py
+++ meal.py
@@ -3,7 +3,6 @@ def main():
     time = input("What time is it? ")
     convert(time)
 def convert(time):
-     answer = time
      hours, minutes = time.split(":")
 # If time is between 7 and 8, print breakfast time
      if "7.0" <= time <= "8.0":
@@ -19,8 +18,7 @@ def convert(time):
         return float(hours) + float(minutes) / 60
 # Otherwise print the default case
      else:
-      print(" ")
-	  return 0;
+       print(" ")
 
 if __name__ == "__main__":
     main()
