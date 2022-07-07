fd178fa3b2a9714fc1620404536c1cce44a8bb60
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-07 02:35:52 +0000

    automated commit by check50 [check50=True]

--- interpreter.py
+++ interpreter.py
@@ -5,19 +5,31 @@ expression = input("Expression: ")
 x, y, z = expression.split(" ")
 
 # x and z are num1 and num2
-num1 = double(x)
-num2 = double(z)
+num1 = float(x)
+num2 = float(z)
 
+# If y = +, then add the two integers
 if y == "+":
     answer = num1 + num2
-    print(round(answer, 1))
+    print(answer)
+
+# If y = -, then subtract the two integers
 elif y == "-":
     answer = num1 - num2
-    print(round(answer, 1))
+    print(answer)
+
+# If y = *, then multiply the two integers
 elif y == "*":
     answer = num1 * num2
-    print(round(answer, 1))
+    print(answer)
+
+# If y = /, then divide the two integers
 elif y == "/":
-    result = num1 / num2
-    print(round(answer, 1))
+    answer = num1 / num2
+    print(answer)
+
+# Display the answer
+else:
+    answer = " "
+    print(answer)
 
