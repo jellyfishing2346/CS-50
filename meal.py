e433f1449abe7879d42b4e291c385fe257d0036c
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-07 15:16:21 +0000

    automated commit by check50 [check50=True]

--- meal.py
+++ meal.py
@@ -8,13 +8,13 @@ def convert(time):
      minutes = float(minutes)
      timeNumber = hours +  (minutes / 60)
 # If time is between 7 and 8, print breakfast time
-     if 7.0 <= time <= 8.0:
+     if 7.0 <= timeNumber <= 8.0:
         print("breakfast time")
 # If time is between 12 and 13 , print lunch time
-     elif 12.0 <= time <= 13.0:
+     elif 12.0 <= timeNumber <= 13.0:
         print("lunch time")
 # If time is between 18 and 19, print dinner time
-     elif 18.0 <= time <= 19.0:
+     elif 18.0 <= timeNumber <= 19.0:
         print("dinner time")
 
 if __name__ == "__main__":
