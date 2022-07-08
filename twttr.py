a64d5028b11d4f29e33689ece404414faa1a6ee3
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-08 20:38:43 +0000

    automated commit by check50 [check50=True]

---
+++ twttr.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter some input
+text = input("Input: ")
+
+
+text = text.replace('a', '')
+text = text.replace('i', '')
+text = text.replace('e', '')
+text = text.replace('o', '')
+text = text.replace('u', '')
+
+print("Output: ", text)
