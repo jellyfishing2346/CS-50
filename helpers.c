0f3e86dc1dde8a7a4a88cfbb7a2d737ff0c3896b
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-10 16:25:08 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -28,9 +28,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         // Evaluate each column by the width
         for (int count = 0; count < width; count++)
         {
-            sepiaRed = round(0.393 * image[index][count].rgbtRed + 0.769 * image[index][count].rgbtBlue + 0.189 * image[index][count].rgbtGreen);
-            sepiaBlue = round(0.393 * image[index][count].rgbtRed + 0.769 * image[index][count].rgbtBlue + 0.189 * image[index][count].rgbtGreen);
-            sepiaGreen = round(0.393 * image[index][count].rgbtRed + 0.769 * image[index][count].rgbtBlue + 0.189 * image[index][count].rgbtGreen);
+            sepiaRed = .393 * image[index][count].rgbtRed + .769 * image[index][count].rgbtGreen + .189 * image[index][count].rgbtBlue;
+         	sepiaGreen = .349 * image[index][count].rgbtRed + .686 * image[index][count].rgbtGreen + .168 * image[index][count].rgbtBlue;
+			sepiaBlue = .272 * image[index][count].rgbtRed + .534 * image[index][count].rgbtGreen + .131 * image[index][count].rgbtBlue;
             image[index][count].rgbtRed = sepiaRed > 255 ? 255: sepiaRed;
             image[index][count].rgbtGreen = sepiaGreen > 255 ? 255: sepiaGreen;
             image[index][count].rgbtBlue = sepiaBlue > 255 ? 255: sepiaBlue;
