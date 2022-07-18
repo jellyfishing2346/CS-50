be4721e57751f9c816d6ece61a1898fff9a4651c
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-18 02:31:15 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -5,63 +5,60 @@
 int main(int argc, char *argv[])
 {
     // Declare the following variables
-    int count = 0;
-    const int BLOCK_SIZE = 512;
-    char *fileInfo = malloc(8 * sizeof(char));
-    typedef unsigned char BYTE;
-    FILE *image = NULL;
-    BYTE buffer[BLOCK_SIZE];
-  
-    // If the argument character is not 2, execute the following statement
+    typedef uint8_t BYTE;
+    
+    // If the character argument is not 2, execute this statement
     if (argc != 2)
     {
         printf("Usage: ./recover IMAGE\n");
-        return 1; 
+        return 1;
     }
-   
-    // Open the memory card.raw
-    FILE *file = fopen(argv[1], "r");
     
-    // Determine if file pointer is NULL
+    // Open the file
+    FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
-        printf("The file is not opened!\n");
-        return 0;
+        printf("This file is not opening %s.\n", argv[1]);
     }
-  
     
-    while (fread(buffer, BLOCK_SIZE, sizeof(char), file))
+    // Set image pointer to NULL
+    FILE *imgptr = NULL;
+    
+    // Memory card
+    BYTE buffer[512];
+    
+    // Count the number of images that have been retrieved
+    int count = 0;
+    
+    // Hold the filename via string
+    char fileInfo[8] = {0};
+    
+    // This function reads the memory card
+    while(fread(buffer, sizeof(BYTE) * 512, 1, file) == 1)
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xff && buffer[2] == 0xf0 && (buffer[3] & 0xf0) == 0xe0)
+        // Determine if jpeg exists
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // If the number of images found is 0
-            if (count == 0)
+            // If the image pointer is not null
+            if (imgptr != NULL)
             {
-                sprintf(fileInfo, "%03i.jpg", count);
-                image = fopen(fileInfo, "w");
-                count++;
+                fclose(file);
             }
+            sprintf(fileInfo, "w");
             
-            // Otherwise print default statement
-            else
-            {
-                fclose(image);
-                sprintf(fileInfo, "%03i.jpg", count);
-                image = fopen(fileInfo, "w");
-                count++;
-            }
+            // Open via the new image pointer
+            imgptr = fopen(fileInfo, "w");
         }
         
-        // If the number of images found is not 0
-        else if (count != 0)
+        // Write the file if the file doesn't already exist
+        if (imgptr != NULL)
         {
-            fwrite(buffer, BLOCK_SIZE, sizeof(char), image);
+            fclose(imgptr);
         }
-    
+        
+        // Close the file
+        fclose(inptr);
     }
-    free(fileInfo);
-    fclose(file);
-    fclose(image);
     
     return 0; 
 }
