df87a756e6aaad76dad35ddc0906886f0910efb1
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-19 16:43:11 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -2,22 +2,17 @@
 from cs50 import get_int
 while True:
     try:
-       height = int(input("Height: "))
-       if height >= 1 and height <= 8:
-        break
-       except:
+        height = int(input("Height: "))
+        if (height >= 1) and (height <= 8):
+            break
+    except:
         print("", end="")
 # The number of spaces accounted
 spaces = 1
 # Account index for rows and count for columns
 for count in range(height):
     for spaces in range(height - count - 1):
-        print("", end="")
+        print(" ", end="")
     for index in range(count + 1):
         print("#", end="")
     print()
-
-
-
-
-
