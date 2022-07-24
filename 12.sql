935c3986fb4adde199fc1043679f7f620b71eac2
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-24 19:15:03 +0000

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,5 +1,5 @@
 SELECT movies.title FROM people
-JOIN stars ON people.id = stars.people_id
+JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE people.name = "Johnny Depp" AND movies.title IN(
     SELECT movies.title FROM people
