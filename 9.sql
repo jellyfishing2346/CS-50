fbb24ca5a55eda83ce3d2d0b4607bd2597426bf4
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-24 18:48:07 +0000

    automated commit by check50 [check50=True]

--- 9.sql
+++ 9.sql
@@ -1,5 +1,5 @@
 SELECT name FROM people
 JOIN stars ON stars.person_id = people.id
-JOIN stars ON stars.movie_id = movies.id
+JOIN movies ON stars.movie_id = movies.id
 WHERE movies.year = 2004
 ORDER BY people.birth
