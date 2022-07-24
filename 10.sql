94c41271665e0e78e25f60d213b4e16f6d96c908
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-24 18:51:10 +0000

    automated commit by check50 [check50=True]

--- 10.sql
+++ 10.sql
@@ -1,4 +1,4 @@
 SELECT name FROM people
 JOIN directors ON people.id = directors.person_id
-JOIN ratings ON directors.movies_id = ratings.movie_id
+JOIN ratings ON directors.movie_id = ratings.movie_id
 WHERE ratings.rating >= 9.0
