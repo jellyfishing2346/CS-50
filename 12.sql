c3c8ae6eabb0067c97ed08ce4235ce3fab2e3f16
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-24 19:11:28 +0000

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,4 +1,8 @@
 SELECT movies.title FROM people
 JOIN stars ON people.id = stars.people_id
 JOIN mvoies ON stars.movie_id = movies.id
-WHERE people
+WHERE people.name = "Johnny Depp" AND movies.title IN(
+    SELECT movies.title FROM people
+    JOIN stars ON people.id = stars.person_id
+    JOIN movies ON stars.movie_id = movies.id
+    where people.name = "Helena Bonham Carter")
