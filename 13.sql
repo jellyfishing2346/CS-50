053646b2724b27aaa30ef05cb67382633be96d50
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-07-24 19:20:36 +0000

    automated commit by check50 [check50=True]

--- 13.sql
+++ 13.sql
@@ -2,5 +2,7 @@ SELECT distinct(name) FROM people
 JOIN stars on people.id = stars.person_id
 JOIN movies on stars.movie_id = movies.id
 WHERE movies.title IN(
-    SELECT distinct(movies.title) FROM peopl
-)
+    SELECT distinct(movies.title) FROM people
+    JOIN stars on people.id = stars.person_id
+    JOIN movies on stars.movie_id = movies.id
+    WHERE people.name = "Kevin Bacon" AND people.birth = 1958 AND people.name != "Kevin Bacon")
