248019866b381196d6cc9274d05f7c989d96e728
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-09 17:39:10 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -118,15 +118,15 @@ def login():
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 400)
-
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
         return redirect("/")
+
+        # Ensure username exists and password is correct
+    if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 400)
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
