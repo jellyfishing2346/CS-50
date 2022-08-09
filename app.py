0d4fcf0456776b6aef994d3b08b06b8653b9121c
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-09 01:54:22 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def login():
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
-        return redirect("/")
+        return redirect("/index")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
