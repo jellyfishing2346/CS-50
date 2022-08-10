519eed74677f58839be45087bcac75715cc6718c
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-10 03:20:19 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -107,7 +107,6 @@ def login():
 
     # User reached route via GET (as by submitting a form via POST)
     if request.method == "POST":
-        print(request.form.get('username'))
 
         # Ensure username was submitted
         if not request.form.get("username"):
@@ -121,20 +120,19 @@ def login():
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(pwhash, password)):
             print("Bad UN/PW", flush=True)
             return apology("invalid username and/or password", 400)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
+        # Redirect user to home page
+        return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-
-    # Redirect user to home page
-    return redirect("/")
 
 
 @app.route("/logout")
