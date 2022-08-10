e5f602c57a00e3b2f848f960a743c917858b9b67
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-10 18:44:12 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def login():
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
-   # else:
+    else:
         return render_template("login.html")
 
 
