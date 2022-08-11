44de1efd1086ed869e3f7a6c6660f82e6b0effcb
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-11 01:54:19 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def login():
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 400)
 
         # Remember which user has logged in
