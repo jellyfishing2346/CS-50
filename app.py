a92ab041e610a121988045b7c0d85178770ab2f1
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-14 16:17:24 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def buy():
         else:
             db.execute("UPDATE users SET cash = cash - ? WHERE id = ?", sharePrice, session["user_id"],
             )
-            db.execute( "INSERT INTO users (userID, symbol, shares, price, operation) VALUES (?, ?, ?, ?, ?)", session["user_id"], dollarSymbol.upper("symbol"),numShares, sharePice["price"], "buy",
+            db.execute( "INSERT INTO users (userID, symbol, shares, price, operation) VALUES (?, ?, ?, ?, ?)", session["user_id"], dollarSymbol["symbol"],numShares, sharePrice["price"], "buy",
             )
 
             flash("Transaction successful")
