6dc57347406626b0dd60bca58b241d27e7c1e4a3
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-14 18:02:01 +0000

    automated commit by check50 [check50=True]

--- plates.py
+++ plates.py
@@ -7,22 +7,30 @@ def main():
 
 
 def is_valid(s):
-    if len(s) < 2 or len(s) > 6:
-        return False
-    if s[0].isalpha() == False or s[1].isalpha() == False:
-        return False
-    index = 0
-    while index < len(s):
-        if s[index].isalpha() == False
-            if s[index] == '0':
-                return False
-            else:
+    lengthInfo = len(s)
+    if lengthInfo >= 2 and lengthInfo <= 6:
+        for letterInfo in s:
+            if not s.isalnum():
                 break
-    index += 1
-    for character in s:
-        if character in ['.', ' ', '!', '?']:
-            return False
-
-    return True
+            if s[0:2].isalpha():
+                middleIndex = s[1::-1]
+                if middleIndex.isnumeric() and middleIndex.find(0):
+                    break
+                zero = s.find("0") - 1
+                if s[-(zero)].isdigit():
+                    for index in s:
+                        if index.digit():
+                            if index.startswith('0'):
+                                return False
+                            else
+                                return True
+                if s[-2].isdigit() and s[-1].isalpha():
+                    break
+                elif s[-2].isdigit():
+                    return True
+                elif s.isalpha():
+                    return True
+            else:
+                return False
 
 main()
