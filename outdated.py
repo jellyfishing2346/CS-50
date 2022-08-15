a34b38a8d411085e13bc37658d03cfc8f6edebea
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-15 19:04:42 +0000

    automated commit by check50 [check50=True]

---
+++ outdated.py
@@ -0,0 +1,54 @@
+# Create a month array
+numMonth = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+# Design a forever loop to go through the date's proper format with month array
+while True:
+    date = input("Date: ")
+    if "/" in date:
+        date = date.split("/")
+        try:
+            day = int(date[1])
+            month = int(date[0])
+        except ValueError:
+            # Conversion of string letters to integer numbers in progress
+            pass
+        else:
+            day = int(date[1])
+            year = date[2]
+        if day > 31:
+            continue
+        elif month > 12:
+            continue
+        year = date[2]
+        month = date[0]
+        day = date[1]
+        print(f"{year}-{month.zfill(2)}-{day.zfill(2)}")
+        break
+    elif " " in date:
+        date = date.split(",")
+        year = date[1]
+        (month, day) = date[0].split(" ")
+        try:
+            month = numMonth.index(month) + 1
+            day = int(day)
+        except IndexError:
+            pass
+        except ValueError:
+            pass
+        else:
+            if day > 31:
+                continue
+            print(f"{year}-{month:02)}-{day:02}")
+            break
