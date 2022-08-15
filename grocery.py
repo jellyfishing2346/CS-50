cc76669876e2b858c304d7024fd3fd90beb8bd86
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-15 03:05:43 +0000

    automated commit by check50 [check50=True]

--- grocery.py
+++ grocery.py
@@ -1,24 +1,19 @@
-# Create a grocery array and list dictionary
-groceryArray = []
-list = {}
+# Create a dictionary called groceries
+  groceryDict = {}
 
-# Design a while loop to loop through the grocery dictionary
-while True:
-    try:
-        # Retrieve the user's input
-        itemCollect = input("")
-        itemCollect = itemCollect.upper()
-        # Going through the grocery array
-        groceryArray.append(itemCollect)
-        groceryArray.sort()
-    except EOFError:
-        for itemCollect in groceryArray:
-            if itemCollect in list:
-                list[itemCollect] += 1
-            else:
-                list[itemCollect] = 1
-            for index in list:
-                print(str(list[index]) + " " + index)
-            break
-        else:
-            continue
+# Use a foreverloop to go through that dictionary
+  while True:
+      try:
+          groceryDict = input("")
+      except EOFError:
+          # Determine if the dictionary is empty
+          for (index, itemCollect) in sorted(groceries.items()):
+              print(index, itemCollect)
+          break
+      groceryDict = groceryDict.strip().upper()
+      try:
+          [grocery]
+      except KeyError:
+          groceries[grocery] = 1
+      else:
+          groceries[grocery] += 1
