159c0d73c86f810336980e1547cb8472635ee465
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-15 17:54:14 +0000

    automated commit by check50 [check50=True]

--- grocery.py
+++ grocery.py
@@ -1,14 +1,21 @@
+# Create a dictionary called grocery
 grocery = {}
-while True:
-    try:
-        item = input()
-        if item in grocery:
-            grocery[item] += 1
-        else:
-            grocery[item] = 1
-    except EOFError as e:
-        break
 
-for grocery, count in groceries.items():
-    print(count, grocery)
-    
+# Design a forever loop
+while True:
+     try:
+        item = input().upper()
+     except KeyboardInterrupt:
+            print("Interruption/n")
+     # Check if the item is already in the dictionary, add 1 to the count
+     if item not in grocery:
+            grocery[item] = 1
+      # Otherwise take the item to be added as a new entry to the dictionary
+     else:
+        grocery[item] += 1
+     try:
+      # Create a list variable
+      groceryList = sorted(dict.items(grocery))
+     except EOFError:
+         for count, index in groceryList:
+            print(index, count, item)
