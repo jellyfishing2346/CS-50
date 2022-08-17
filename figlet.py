6960bdf1a68056a84d6c89fc7496a7c90b978e07
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-17 16:57:33 +0000

    automated commit by check50 [check50=True]

--- figlet.py
+++ figlet.py
@@ -1,17 +1,36 @@
 import sys
-from random import choice
+import random
 from pyfiglet import Figlet
 
+figlet = Figlet()
 
-num_args = len(sys.argv)
-
-if num_args == 1:
-    font = choice(Figlet().getFonts())
-elif num_args == 3 and sys.argv[1] in ["-f", "--font"]:
-    font = sys.argv[2]
+# Evaluating the given random text
+if len(sys.argv) == 1:
+    isFont = True
+elif len(sys.argv) == 3:
+    isFont = False
 else:
-    print('Invalid usage')
+    print("Invalid usuage")
     sys.exit(1)
 
-Figlet.setFont(font=font)
-print(Figlet.renderText(input('Input: ')))
+# This will list the fonts that are avaiable from the figlet package
+figlet.getFonts()
+
+if isFont == False:
+    try:
+        fontUser = figlet.setFont(font=sys.argv[2])
+    except:
+        print("Invalid usuage")
+        sys.exit(1)
+else:
+    fontUser = random.choice(figlet.getFonts())
+
+# Get the user to input some random text
+text = input("Input: ")
+
+# Output text in the font with a code using s as a str
+response = figlet.renderText(text)
+
+# Display the output text
+print("Output: ")
+print(response)
