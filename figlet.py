febf3057a8d47a5ec5bb0bb6cf8cd8ab5bf26b41
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-17 22:00:54 +0000

    automated commit by check50 [check50=True]

--- figlet.py
+++ figlet.py
@@ -9,7 +9,7 @@ if len(sys.argv) == 1:
     isFont = True
 elif len(sys.argv) == 3:
     isFont = False
-elif len(sys.argv[1]) == "-f" or len(sys.argv[1]) == "--font":
+elif not len(sys.argv[1]) == "-f" or len(sys.argv[1]) == "--font":
     isFont = False
 else:
     print("Invalid usuage")
