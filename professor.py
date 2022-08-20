b868783dae74699ac9536cff0acc66cce356bba8
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-20 02:54:45 +0000

    automated commit by check50 [check50=True]

--- professor.py
+++ professor.py
@@ -1,58 +1,61 @@
 import random
-import sys
 
 # Create a main function to determine to get the user's score
 def main():
     levelInfo = retrieveLevel()
-
-    mistakes = 1
-    scoreTrack = 0
-
-    for track in range(10):
-        index = randomIntegers(levelInfo)
-        for check in range(1):
-            count = randomIntegers(levelInfo)
-
-            result = index + count
-            equation = input(f"{index} + {count} = ")
-
-            if int(equation) == result:
-                scoreTrack += 1
-
-            while int(equation) != result:
-                mistakes += 1
-                print("EEE")
-                equation = input(f"{index} + {count} = ")
-                if mistakes >= 3:
-                    print(result)
-                    sys.exit("Score: " + str(scoreTrack))
-
-    print("Score: " + str(scoreTrack))
+    correct = randomIntegers(levelInfo)
+    print("Score: ", correct)
 
 # This function is to know what level the user is on
 def retrieveLevel():
-    levelInfo = input("Level: ")
-
-    if levelInfo.isalpha() or int(levelInfo) <= 0 or int(levelInfo) > 3:
-        input("Level: ")
-    else:
-        levelInfo = int(levelInfo)
-        for game in [1,2,3]:
-            if levelInfo == game:
-                return levelInfo
+    while True:
+        try:
+            input = int(input("Level: "))
+            if not input in range(1,4):
+                raise ValueError
+            else:
+                return input
+        except ValueError:
+            continue
 
 # Random integers for each level for the game
-def randomIntegers(levelInfo):
-    try:
-        if levelInfo == 1:
-            return random.randint(0,9)
-        elif levelInfo == 2:
-            return random.randint(10,99)
-        elif levelInfo == 3:
-            return random.randint(100,999)
-    except:
-        raise ValueError
-
+def randomIntegers():
+    scoreTrack = 0
+    if levelInfo == 1:
+        for _ in range(10):
+            first = random.randint(10,99)
+            second = random.randint(10,99)
+            rounds = 0
+            while True:
+                if rounds == 3:
+                    print(f'{first} + {second} = {first + second}')
+                    break
+                input = int(input(f'{first} + {second} = '))
+                if input == first + second:
+                    scoreTrack += 1
+                    break
+                else:
+                    print('EEE')
+                    rounds += 1
+                return scoreTrack
+        else:
+            for _ in range(10):
+                first = random.randint(100,999)
+                second = random.randint(109,999)
+                rounds = 0
+                while True:
+                    if rounds == 3:
+                        print(f'{first} + {second} = {first + second}')
+                        break
+                    input = int(input(f'{first} + {second} = '))
+                    if input == first + second:
+                        scoreTrack += 1
+                        break
+                    else:
+                        print('EEE')
+                        rounds += 1
+                return scoreTrack
+                
 # Main function set up
 if __name__ == '__main__':
     main()
