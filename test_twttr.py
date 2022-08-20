f08e77cc03b730378deb6590bdfeaa3473e3519d
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-20 22:12:54 +0000

    automated commit by check50 [check50=True]

--- test_twttr.py
+++ test_twttr.py
@@ -1,4 +1,5 @@
 from twttr import shorten
+import sys
 
 # Creare a fucntion to shorten any given word
 def shortenTest():
@@ -6,3 +7,4 @@ def shortenTest():
     assert shorten("this is your tweet") == "ths s you twt"
     assert shorten("UPPERCASE") == "PPRCS"
     assert shorten("s0mething") == "s0mething"
+sys.exit(1)
