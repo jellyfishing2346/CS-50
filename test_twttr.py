7567dea22dccf3751bbd55455ac6f8f01815e609
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-22 18:02:21 +0000

    automated commit by check50 [check50=True]

--- test_twttr.py
+++ test_twttr.py
@@ -1,8 +1,24 @@
 from twttr import shorten
 
-# Creare a fucntion to shorten any given word
-def shortenTest():
-    assert shorten("testing") == "tsting"
-    assert shorten("this is your tweet") == "ths s you twt"
-    assert shorten("UPPERCASE") == "PPRCS"
-    assert shorten("s0mething") == "s0mething"
+def main():
+    # Call the following test functions
+    test_letter_cases()
+    test_numbers()
+    test_punctuation()
+
+# Test the vowels for upper case and lower case
+def test_letter_cases():
+    assert shorten('twitter') == 'twttr'
+    assert shorten('TWITTER') == 'TWTTR'
+    assert shorten('TwItTeR') == 'TwtTR'
+
+# Test the case for numbers
+def test_numbers():
+    assert shorten('1234') == '1234'
+
+# Test the punctuation
+def test_punctuation():
+    assert shorten('!?.,') == '!?.,'
+
+    if __name__ == "__main__":
+        main()
