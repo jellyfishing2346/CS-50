0a568344e54f4635c8db3ec0241379a920a91b79
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-22 22:18:40 +0000

    automated commit by check50 [check50=True]

--- test_bank.py
+++ test_bank.py
@@ -2,24 +2,24 @@ from bank import value
 
 def main():
     # Call the following test functions
-    returnZero()
-    returnTwenty()
-    returnHundred()
+    test_return_zero()
+    test_return_twenty()
+    test_return_hundred()
 
 # This function deals with conditions that return 0
-def returnZero():
+def test_return_zero():
     assert value('hello, mark') == 0
     assert value('Hello') == 0
     assert value('hello, Mark') == 0
     assert value('Hello, Mark') == 0
 
 # This function deals with conditions that return 20
-def returnTwenty():
+def test_return_twenty():
     assert value('Hi') == 20
     assert value('hey') == 20
 
 # This function deals with conditions that return 100
-def returnHundred():
+def test_return_hundred():
     assert value("What is up bro?") == 100
     assert value('good evening') == 100
 
