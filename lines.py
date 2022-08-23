2f9d61a61d604a27483f2e84c03ff0d41d28c7e4
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-23 15:25:51 +0000

    automated commit by check50 [check50=True]

--- lines.py
+++ lines.py
@@ -2,7 +2,7 @@ import sys
 
 # Main function
 def main():
-    check_commandLine()
+    check_command_line()
     # Open the file
     try:
         with open(sys.argv[1], "r") as fileInfo:
@@ -14,12 +14,12 @@ def main():
     # Loop through the lines to see if # or whitespace is present
     count = 0
     for index in lineInfo:
-        if commandLine_is_empty(index) == True:
+        if check_command_line_is_empty(index) == True:
             count += 1
     print(count)
 
 # Check the command line argument
-def check_commandLine():
+def check_command_line():
     # Check the elements in the command line
     if len(sys.argv) < 2:
         sys.exit("Too few command-line arguments")
@@ -30,7 +30,7 @@ def check_commandLine():
         sys.exit("Not a Python file")
 
 # Check the command line is empty
-def commandLine_is_empty(index):
+def check_command_line_is_empty(index):
     if index.lstrip().startswith('#'):
         return False
     elif index.isspace():
