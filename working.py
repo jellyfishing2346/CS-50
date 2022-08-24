4f3bc5636e22a486bb90a39629624f7cd1c84ae5
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-24 18:02:30 +0000

    automated commit by check50 [check50=True]

--- working.py
+++ working.py
@@ -14,7 +14,7 @@ def convert(s):
         # The first time in 24 hours
         first = time_format(time[1], time[2], time[3])
         # The second time in 24 hours
-        second = time_format(time[4], time[5], time[6])
+        second = time_format(time[5], time[6], time[7])
         return first + " to " + second
     else:
         raise ValueError
@@ -22,15 +22,15 @@ def convert(s):
 # Time format function
 def time_format(hours, minutes, AM_PM):
     if AM_PM == 'PM':
-        if int(hours) == 12:
+        if hours == 12:
             numHours = 12
         else:
-            numHours = int(hours) + 12
+            numHours = str(hours) + 12
     else:
-        if int(hours) == 12:
+        if hours == 12:
             numHours = 0
         else:
-            numHours = int(hours)
+            numHours = hours
     if minutes == None:
         numTime = f"{numHours:02}" + ":00"
     else:
