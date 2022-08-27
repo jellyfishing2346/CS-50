d10280c61560e8942d4dc0ac4a6b3247159d54fc
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-27 18:28:28 +0000

    automated commit by check50 [check50=True]

--- test_seasons.py
+++ test_seasons.py
@@ -1,4 +1,4 @@
-from seasons import check_birthday
+from seasons import check
 
 # Main function
 def main():
@@ -6,9 +6,9 @@ def main():
 
 # This function is to verify a person's birthday info
 def test_check_birthday_info():
-    assert check_birthday("July 3, 1998") == None
-    assert check_birthday("1998-7-3") == None
-    assert check_birthday("1998-07-03") == ("1998", "07", "03")
+    assert check("July 3, 1998") == None
+    assert check("1998-7-3") == None
+    assert check("1998-07-03") == ("1998", "07", "03")
 
 if __name__ == "__main__":
     main()
