3f0a60836843d9e32b74ed4077cd8931d42a890d
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-27 22:04:00 +0000

    automated commit by check50 [check50=True]

--- um.py
+++ um.py
@@ -7,8 +7,9 @@ def main():
 # Count function
 def count(s):
     um = re.findall(r"\b\W*um\W*", s, re.IGNORECASE)
-    return len(um)
+    return um
 
 
 if __name__ == "__main__":
     main()
+          
