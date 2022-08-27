c287a2aaf51363874213a76938bf41c470fc56ff
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-27 18:19:58 +0000

    automated commit by check50 [check50=True]

--- test_seasons.py
+++ test_seasons.py
@@ -1,29 +1,11 @@
-from datetime import date
-import sys
-import re
-import inflect
-
-information = inflect.engine()
+from seasons import check_birthday
 
 # Main function
 def main():
-    dateofBirth = input("Date of Birth: ")
-    try:
-        numYear, numMonth, numDay = check_birthday_info(dateofBirth)
-    except:
-        sys.exit("Invalid date")
-    birthdayInformation = date(int(numYear), int(numMonth), int(numDay))
-    todayDateInfo = date.today()
-    difference = todayDateInfo - birthdayInformation
-    minutes = difference.numDay * 24 * 60
-    response = information.number_to_words(minutes, andword='')
-    print(response.capitalize() + " minutes")
+    test_check_birthday_info()
 
-# Check the person's birthday
-def check_birthday_info(dateofBirth):
-    if re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", dateofBirth):
-        numYear, numMonth, numDay = dateofBirth.split("-")
-        return numYear, numMonth, numDay
-
-if __name__ == "__main__":
-    main()
+# This function is to verify a person's birthday info
+def test_check_birthday_info():
+    assert check_birthday("July 3, 1998") == None
+    assert check_birthday("1998-7-3") == None
+    assert check_birthday("1998-07-03") == ("1998", "07", "03")
