8a9fdddf3e2f19d2c230f930c716b7668216a0ed
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-29 18:51:32 +0000

    automated commit by check50 [check50=True]

--- jar.py
+++ jar.py
@@ -18,15 +18,15 @@ class Jar:
         for _ in range(n):
             self.jars.pop()
 
-     @capacity.setter
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @capacity.setter
     def capacity(self, capacity):
         if capacity < 0:
             raise ValueError
         self.capacity = capacity
-
-    @property
-    def capacity(self):
-        return self._capacity
 
     @property
     def size(self):
