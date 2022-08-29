4f449ac8a6c98cb20d6c85d010388889b37770ba
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-29 03:27:29 +0000

    automated commit by check50 [check50=True]

--- jar.py
+++ jar.py
@@ -2,7 +2,6 @@ class Jar:
     def __init__(self, capacity=12):
         self._capacity = capacity
         self.jars = []
-        return Jar(capacity, jars)
 
     def __str__(self):
         return self.size * '🍪'
@@ -38,3 +37,4 @@ jar.deposit(5)
 print(jar)
 jar.withdraw(2)
 print(jar)
+return capacity(jars, capacity)
