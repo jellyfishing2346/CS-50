33e9db7d7e7b02a32bac96812aae33c059e8bb31
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2022-08-30 21:49:27 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def buy():
     """Buy shares of stock"""
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        dollarSymbol = request.form.get("symbol")
+        dollarSymbol = request.form.get(" symbol")
         stockPrice = lookup(dollarSymbol)
         numShares = request.form.get("shares")
         cash = db.execute("SELECT users FROM hash WHERE id = ? ", session["user_id"])[0]["cash"]
