ca55b4949610966f65d16a35c8eec21ac9aedd75
jellyfishing2346 <jellyfishing2346@users.noreply.github.com>
2023-11-05 09:18:09 -0500

    automated commit by submit50

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -13,29 +14,20 @@ def initial_state():
     """
     Returns starting state of the board.
     """
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
+    return [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]]
 
 
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
+    counter = 0
+    for row in board:
+       for cell in row:
+          if cell is not EMPTY:  # assuming EMPTY is a constant defined elsewhere
+             counter += 1
+    return X if counter % 2 == 0 else O  # assuming X and O are constants defined elsewhere
 
-    players = 0
-    turns = 0
-
-    for index in range(len(board)):
-        for count in range(len(board)):
-            if board[index][count] == players:
-                players += 1
-            if board[index][count] == turns:
-                turns += 1
-        if players > turns:
-            return turns
-        else:
-            return players
 
 
 def actions(board):
@@ -43,19 +35,23 @@ def actions(board):
     Returns set of all possible actions (i, j) available on the board.
     """
     allOutcomes = set()
+    row = 0
+    column = 0
 
-    for index in range(len(board)):
-        for count in range(len(board[row])):
-            if board[row][count] == EMPTY:
-                allOutcomes.add((index, column))
-        return allOutcomes
+    for row in range(len(board)):
+        for column in range(len(board[row])):
+            if board[row][column] == EMPTY:
+                allOutcomes.add((row, column))
+    # return should be here, outside of the loops
+    return allOutcomes
+
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    if action not in actions(board):
-        raise Exception('This action is not avaiable at all')
+    if tuple(action) not in actions(board):
+        raise Exception("This action is not avaiable at all")
     boardCopy = copy.deepcopy(board)
 
     index, count = action
@@ -65,16 +61,24 @@ def result(board, action):
 
 def checkIndex(board, players):
     for index in range(len(board)):
-        if board[index][0] == players and board[index][1] == players and board[index][2] == players:
+        if (
+            board[index][0] == players
+            and board[index][1] == players
+            and board[index][2] == players
+        ):
             return True
-        return False
+    return False
 
 
 def checkCount(board, players):
     for count in range(len(board)):
-        if board[count][0] == players and board[count][1] == players and board[count][2] == players:
+        if (
+            board[count][0] == players
+            and board[count][1] == players
+            and board[count][2] == players
+        ):
             return True
-        return False
+    return False
 
 
 def topBottom(board, players):
@@ -83,7 +87,7 @@ def topBottom(board, players):
         for count in range(len(board)):
             if index == count and board[index][len(board) - index - 1] == players:
                 counting += 1
-            return counting == 3
+    return counting == 3
 
 
 def bottomTop(board, players):
@@ -92,17 +96,27 @@ def bottomTop(board, players):
         for count in range(len(board)):
             if index == count and board[index][count] == players:
                 counting += 1
-            return counting == 3
+    return counting == 3
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    if checkIndex(board, players) or checkCount(board, players) or topBottom(board, players) or bottomTop(board, players):
-        return player
-    elif checkIndex(board, turns) or checkCount(board, turns) or topBottom(board, turns) or bottomTop(board, turns):
-        return turns
+    if (
+        checkIndex(board, 'X')
+        or checkCount(board, 'X')
+        or topBottom(board, 'X')
+        or bottomTop(board, 'X')
+    ):
+        return 'X'
+    elif (
+        checkIndex(board, 'O')
+        or checkCount(board, 'O')
+        or topBottom(board, 'O')
+        or bottomTop(board, 'O')
+    ):
+        return 'O'
     else:
         return None
 
@@ -111,25 +125,23 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    if winner(board) == players:
+    winnerFound = winner(board)
+    if winnerFound != None:
         return True
-    elif winner(board) == turns:
-        return True
-
-    for index in range(len(board)):
-        for count in range(len(board[index])):
-            if board[index][count] == EMPTY:
-                return False
-        return True
+    for r in board:
+        if EMPTY in r:
+            return False
+    return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    if winner(board) == players:
+
+    if winner(board) == X:
         return 1
-    elif winner(board) == turns:
+    elif winner(board) == O:
         return -1
     else:
         return 0
@@ -145,7 +157,7 @@ def maxValue(board):
 
 
 def minValue(board):
-    value = -math.inf
+    value = math.inf
     if terminal(board):
         return utility(board)
     for action in actions(board):
@@ -157,15 +169,18 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
+
     if terminal(board):
         return None
-    elif player(board) == players:
-        work = []
-        for action in actions(board):
+    players = player(board)
+    work = []
+    for action in actions(board):
+        if players == player(board):
             work.append([minValue(result(board, action)), action])
-        return sorted(plays, key=lambda index: index[0], reverse=True)[0][1]
-    elif player(board) == turns:
-        work = []
-        for action in actions(board):
+        else:
             work.append([maxValue(result(board, action)), action])
-        return sorted(plays, key=lambda index: index[0], reverse=True)[0][1]
+    if players == player(board):
+        return max(work, key=lambda numIndex: numIndex[0])[1]
+    else:
+        return min(work, key=lambda numIndex: numIndex[0])[1]
+
